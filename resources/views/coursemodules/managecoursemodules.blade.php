a1c28ba523ea7b4acd0cd37677f7eca694e5cb57
Fred Nyachomo <nyachomofred@gmail.com>
2026-01-15 09:52:11 +0300

    Fixing the editor

--- resources/views/coursemodules/managecoursemodules.blade.php
+++ resources/views/coursemodules/managecoursemodules.blade.php
@@ -177,17 +177,14 @@
                                                                     <!-- text input -->
                                                                     <div class="form-group">
                                                                         <label>What to Learn<sup>*</sup></label>
-                                                                        <textarea class="textarea"><?php echo$module->module_content?></textarea>
+                                                                        <textarea class="textarea1"><?php echo$module->module_content?></textarea>
        
                                                                         
                                                                     </div>
 
 
                                                                     <div class="box">
-                                                                            <div class="box-header">
-                                                                                    <h3 class="box-title">Bootstrap WYSIHTML5
-                                                                                        <small>Simple and fast</small>
-                                                                                    </h3>
+                                                                            
                                                                             <!-- tools box -->
                                                                                     <div class="pull-right box-tools">
                                                                                         <button type="button" class="btn btn-default btn-sm" data-widget="collapse" data-toggle="tooltip"
@@ -197,16 +194,14 @@
                                                                                                 title="Remove">
                                                                                         <i class="fa fa-times"></i></button>
                                                                                     </div>
-                                                                                    <!-- /. tools -->
-                                                                            </div>
-                                                                    <!-- /.box-header -->
+                                                                           
                                                                             <div class="box-body pad">
-                                                                            <form>
-                                                                                <textarea class="textarea" placeholder="Place some text here"
-                                                                                        style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
-                                                                            </form>
+                                                                            
+                                                                                    <textarea class="textarea" placeholder="Place some text here"
+                                                                                            style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"><?php echo$module->module_content?></textarea>
+                                                                              
                                                                             </div>
-                                                                </div>
+                                                                     </div>
 
 
 
