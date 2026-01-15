73a6e58d0057323e004bd0ae449098d387a43d0f
Fred Nyachomo <nyachomofred@gmail.com>
2026-01-15 10:05:47 +0300

    fixing the editor

--- resources/views/coursemodules/managecoursemodules.blade.php
+++ resources/views/coursemodules/managecoursemodules.blade.php
@@ -177,34 +177,8 @@
                                                                     <!-- text input -->
                                                                     <div class="form-group">
                                                                         <label>What to Learn<sup>*</sup></label>
-                                                                        <textarea class="textarea1"><?php echo$module->module_content?></textarea>
-       
-                                                                        
+                                                                        <textarea class="textarea"><?php echo$module->module_content?></textarea>
                                                                     </div>
-
-
-                                                                    <div class="box">
-                                                                            
-                                                                            <!-- tools box -->
-                                                                                    <div class="pull-right box-tools">
-                                                                                        <button type="button" class="btn btn-default btn-sm" data-widget="collapse" data-toggle="tooltip"
-                                                                                                title="Collapse">
-                                                                                        <i class="fa fa-minus"></i></button>
-                                                                                        <button type="button" class="btn btn-default btn-sm" data-widget="remove" data-toggle="tooltip"
-                                                                                                title="Remove">
-                                                                                        <i class="fa fa-times"></i></button>
-                                                                                    </div>
-                                                                           
-                                                                            <div class="box-body pad">
-                                                                            
-                                                                                    <textarea class="textarea" placeholder="Place some text here"
-                                                                                            style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"><?php echo$module->module_content?></textarea>
-                                                                              
-                                                                            </div>
-                                                                     </div>
-
-
-
 
                                                                 </div>
 
